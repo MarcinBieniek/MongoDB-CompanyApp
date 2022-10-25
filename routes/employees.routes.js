7f28a9ba421a62c0e7b5bd7025397733dc88102b
Marcin Bieniek <marcingim@gmail.com>
2022-10-25 14:37:58 +0200

    Add products and employees mongoDB endpoints

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,34 +1,56 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
+// endpoint 1 - get all - tested
 router.get('/employees', (req, res) => {
-  res.json(db.employees);
+  req.db.collection('employees').find().toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data); 
+  });
 });
 
+// endpoint 2 - random - tested
 router.get('/employees/random', (req, res) => {
-  res.json(db.employees[Math.floor(Math.random() * db.length)]);
+  req.db.collection('employees').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  });
 });
 
+//endpoint 3 - get document by id - tested
 router.get('/employees/:id', (req, res) => {
-  res.json(db.employees.find(item => item.id == req.params.id));
+  req.db.collection('employees').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'Not found' });
+    else res.json(data);
+  });
 });
 
+//endpoint 4 - post new element - tested
 router.post('/employees', (req, res) => {
   const { firstName, lastName } = req.body;
-  db.employees.push({ id: 3, firstName, lastName })
-  res.json({ message: 'OK' });
+  req.db.collection('employees').insertOne({ firstName, lastName }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  })
 });
 
+//endpoint 5 - edit document - tested
 router.put('/employees/:id', (req, res) => {
   const { firstName, lastName } = req.body;
-  db = db.employees.map(item => (item.id == req.params.id) ? { ...item, firstName, lastName } : item );
-  res.json({ message: 'OK' });
+  req.db.collection('employees').updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName, lastName }}, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
+//endpoint 6 - delete document - tested
 router.delete('/employees/:id', (req, res) => {
-  db = db.employees.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('employees').deleteOne({ _id: ObjectId(req.params.id) }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
 module.exports = router;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,36 +1,56 @@
-// post.routes.js
-
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
+//endpoint 1 - get all documents - tested
 router.get('/products', (req, res) => {
-  res.json(db.products);
+  req.db.collection('products').find().toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data); 
+  });
 });
 
+//endpoint 2 - get random document - tested
 router.get('/products/random', (req, res) => {
-  res.json(db.products[Math.floor(Math.random() * db.length)]);
+  req.db.collection('products').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  });
 });
 
+//endpoint 3 - get document by id - tested
 router.get('/products/:id', (req, res) => {
-  res.json(db.products.find(item => item.id == req.params.id));
+  req.db.collection('products').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else if(!data) res.status(404).json({ message: 'Not found' });
+    else res.json(data);
+  });
 });
 
+//endpoint 4 - add new document - tested
 router.post('/products', (req, res) => {
   const { name, client } = req.body;
-  db.products.push({ id: 3, name, client })
-  res.json({ message: 'OK' });
+  req.db.collection('products').insertOne({ name, client }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  })
 });
 
+//endpoint 5 - edit document - tested
 router.put('/products/:id', (req, res) => {
   const { name, client } = req.body;
-  db = db.products.map(item => (item.id == req.params.id) ? { ...item, name, client } : item );
-  res.json({ message: 'OK' });
+  req.db.collection('products').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name, client }}, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
+//endpoint 6 - delete document - tested
 router.delete('/products/:id', (req, res) => {
-  db = db.products.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('products').deleteOne({ _id: ObjectId(req.params.id) }, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
 });
 
 module.exports = router;
