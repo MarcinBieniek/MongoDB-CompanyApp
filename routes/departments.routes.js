9c754de4234c3f10f2fe3f9f4cff12dcf6de3411
Marcin Bieniek <marcingim@gmail.com>
2022-10-26 00:37:47 +0200

    Add mongoose to departments enpoints

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,42 +1,74 @@
 const express = require('express');
 const router = express.Router();
-const ObjectId = require('mongodb').ObjectId;
+const Department = require('../models/department.model');
 
-// endpoint 1 - get all
-router.get('/departments', (req, res) => {
-  req.db.collection('departments').find().toArray((err, data) => {
-    if(err) res.status(500).json({ message: err });
-    else res.json(data); 
-  });
+//endpoint 1 - get all - mongooose - tested
+router.get('/departments', async (req, res) => {
+  try {
+    res.json(await Department.find());
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-// endpoint 2 - random (to do later)
+//endpoint 2 - random - mongooose - tested
+router.get('/departments/random', async (req, res) => {
 
-router.get('/departments/random', (req, res) => {
-  req.db.collection('departments').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
-    if(err) res.status(500).json({ message: err });
-    else res.json(data[0]);
-  });
+  try {
+    const count = await Department.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Department.findOne().skip(rand);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*router.get('/departments/random', (req, res) => {
   res.json(db.departments[Math.floor(Math.random() * db.length)]);
 });*/
 
-// endpoint 3 - get by id 
-router.get('/departments/:id', (req, res) => {
-  req.db.collection('departments').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
-    if(err) res.status(500).json({ message: err });
-    else if(!data) res.status(404).json({ message: 'Not found' });
-    else res.json(data);
-  });
+// endpoint 3 - get by id - mongooose - tested
+router.get('/departments/:id', async (req, res) => {
+
+  try {
+    const dep = await Department.findById(req.params.id);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
  /*router.get('/departments/:id', (req, res) => {
   res.json(db.departments.find(item => item.id == req.params.id));
 });*/
 
-// endpoint 4 - post new element 
+// endpoint 4 - post new element - mongooose - tested
+
+router.post('/departments', async (req, res) => {
+
+  try {
+
+    const { name } = req.body;
+    const newDepartment = new Department({ name: name });
+    await newDepartment.save();
+    res.json({ message: 'OK' });
+
+  } catch(err) {
+    res.status(500).json({ message: err });
+  }
+
+});
+
+/* 
+2 version - mongoDB
 router.post('/departments', (req, res) => {
   const { name } = req.body;
   req.db.collection('departments').insertOne({ name: name }, err => {
@@ -45,19 +77,29 @@ router.post('/departments', (req, res) => {
   })
 });
 
-/* router.post('/departments', (req, res) => {
+1 version - classic db
+router.post('/departments', (req, res) => {
   const { name } = req.body;
   db.departments.push({ id: 3, name })
   res.json({ message: 'OK' });
 });*/
 
-// endpoint 5 - edit document
-router.put('/departments/:id', (req, res) => {
+// endpoint 5 - edit document -  - mongooose - tested
+router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
-  req.db.collection('departments').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name }}, err => {
-    if(err) res.status(500).json({ message: err });
-    else res.json({ message: 'OK' });
-  });
+
+  try {
+    const dep = await Department.findById(req.params.id);
+    if(dep) {
+      await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /* router.put('/departments/:id', (req, res) => {
@@ -66,13 +108,21 @@ router.put('/departments/:id', (req, res) => {
   res.json({ message: 'OK' });
 });*/
 
-//endpoint 6 - delete document
+//endpoint 6 - delete document - mongooose - tested
+router.delete('/departments/:id', async (req, res) => {
 
-router.delete('/departments/:id', (req, res) => {
-  req.db.collection('departments').deleteOne({ _id: ObjectId(req.params.id) }, err => {
-    if(err) res.status(500).json({ message: err });
-    else res.json({ message: 'OK' });
-  });
+  try {
+    const dep = await Department.findById(req.params.id);
+    if(dep) {
+      await Department.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
+
 });
 
 /*router.delete('/departments/:id', (req, res) => {
